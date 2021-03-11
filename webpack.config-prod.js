e1c1cc576132d210ce7ea4d4e38b2e40ab5bd6ab
janimattiellonen <janimatti.ellonen@gmail.com>
2021-03-11 07:09:21 +0200

    Fuxxor

--- webpack.config-prod.js
+++ webpack.config-prod.js
@@ -10,20 +10,20 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const dotenv = require('dotenv')
 const webpack = require('webpack')
-
+/*
 const env = dotenv.config().parsed
 
 const envKeys = Object.keys(env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next])
   return prev
 }, {})
-
+*/
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
   entry: { app: './src/index.js' },
   plugins: [
-    new webpack.DefinePlugin(envKeys),
+    new webpack.DefinePlugin([]),
     new CleanWebpackPlugin(),
     new CompressionPlugin({
       test: /\.js(\?.*)?$/i,

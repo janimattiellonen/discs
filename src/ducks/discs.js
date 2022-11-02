e0dc22de1ad35ae0273444f0ffd475e0a181ea7e
janimattiellonen <janimatti.ellonen@gmail.com>
2022-11-02 20:54:53 +0200

    Removed unused code

--- src/ducks/discs.js
+++ src/ducks/discs.js
@@ -1,19 +1,7 @@
-import { List, Map } from 'immutable';
 import { add, isBefore } from 'date-fns';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import discApi from '../api/disc';
-import { counterSlice, incrementAsync } from '../features/counter/counterSlice';
-
-const FETCH_DISCS = 'jme/discs/FETCH_DISCS';
-const FETCH_DISCS_DONE = 'jme/discs/FETCH_DISCS_DONE';
-const FETCH_DISCS_FAILED = 'jme/discs/FETCH_DISCS_FAILED';
-
-const FETCH_DISC_STATS = 'jme/discs/FETCH_DISC_STATS';
-const FETCH_DISC_STATS_DONE = 'jme/discs/FETCH_DISC_STATS_DONE';
-const FETCH_DISC_STATS_FAILED = 'jme/discs/FETCH_DISC_STATS_FAILED';
-
-const ADD_DISC = 'jme/discs/ADD_DISC';
 
 const initialState = {
     discs: [],
@@ -111,82 +99,5 @@ export const discsSlice = createSlice({
             });
     },
 });
-
-// TODO: Remove
-const foo = (state = initialState, action = {}) => {
-    const { type, payload } = action;
-
-    switch (type) {
-        case FETCH_DISCS:
-            return state.withMutations((map) => map.set('loadingDiscs', true).set('loadingDiscsFailed', false));
-
-        case FETCH_DISCS_DONE:
-            const limit = parseInt(payload.limit, 10);
-            const offset = parseInt(payload.offset, 10);
-            const total = parseInt(payload.total, 10);
-            const count = parseInt(payload.count, 10);
-            const skip = parseInt(payload.skip, 10);
-
-            return state.withMutations((map) =>
-                map
-                    .set('loadingDiscs', false)
-                    .set('loadingDiscsFailed', false)
-                    .set('discs', offset === 0 ? List(payload.discs) : map.get('discs').concat(List(payload.discs)))
-                    .set('limit', limit)
-                    .set('offset', offset)
-                    .set('count', count)
-                    .set('total', total)
-                    .set('skip', skip),
-            );
-
-        case FETCH_DISC_STATS:
-            return state.withMutations((map) => map.set('loadingStats', false).set('loadingStatsFailed', false));
-
-        case FETCH_DISC_STATS_DONE:
-            return state.withMutations((map) =>
-                map.set('loadingStats', false).set('loadingStatsFailed', false).set('stats', Map(payload)),
-            );
-
-        case ADD_DISC:
-            return state;
-
-        default:
-            return state;
-    }
-};
-
-export function fetchDiscs(params) {
-    return (dispatch) => {
-        dispatch({ type: FETCH_DISCS });
-
-        discApi.getDiscs(params).then((results) => {
-            return dispatch({
-                type: FETCH_DISCS_DONE,
-                payload: {
-                    discs: results.data,
-                    total: results.totals.total,
-                    limit: params.limit,
-                    offset: params.offset,
-                    count: results.totals.count,
-                    skip: results.totals.skip,
-                },
-            });
-        });
-    };
-}
-
-// TODO: Remove, not used?
-export function fetchDiscStats() {
-    return (dispatch) => {
-        dispatch({ type: FETCH_DISC_STATS });
-
-        discApi.getStats().then(
-            (stats) => {
-                dispatch({ type: FETCH_DISC_STATS_DONE, payload: stats });
-            },
-            () => dispatch({ type: FETCH_DISC_STATS_FAILED }),
-        );
-    };
-}
 
 export default discsSlice.reducer;
